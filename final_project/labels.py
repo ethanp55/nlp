7a30e393e76f92b3d9f553c35a983768bf68589f
Ethan Pedersen <ethanp55@gmail.com>
2024-11-20 10:50:35 -0700

    Initial data for JHG text classification

---
+++ final_project/labels.py
@@ -0,0 +1,164 @@
+all_classes = [
+    # Building relationships
+    ["Friends?",
+     "Let's form a group",
+     "Let's form a secret group",
+     "{player_names}, would you like to join our group?",
+     "Can {player_names} join your group?",
+     "Send {token_amount} to {player_names}",
+     "Send {token_amount} to {player_names}?",
+     "{player_names} send {token_amount} to {player_names}",
+     "{player_names} send {token_amount} to {player_names}?",
+     "Sending you {token_amount}",
+     "Sending {token_amount} to {player_names}",
+     "Sending you each {token_amount}",
+     "{player_names} need to give",
+     "Add {player_names} to the group?",
+     "Add {player_names} to the group",
+     "To boost {player_names}",
+     "To grow stronger",
+     "To make more friends",
+     "I haven't been receiving",
+     "Talk with {player_names}",
+     "I'll talk with {player_names}",
+     "I talked with {player_names}"],
+
+    # Destroying relationships
+    ["Don't send {token_amount} to {player_names}",
+     "Attack {player_names}",
+     "Attack {player_names}?",
+     "Don't attack {player_names}",
+     "Steal {token_amount}",
+     "Steal {token_amount} from {player_names}",
+     "Stealing {token_amount} from {player_names}",
+     "Drop {player_names} from the group",
+     "Drop {player_names} from the group?",
+     "Replace {player_names} with {player_names}?",
+     "Leave the group?",
+     "{player_names} need to attack {player_names}"],
+
+    # Defense
+    ["I am not stealing",
+     "Keep {token_amount} {player_names}",
+     "Keep {token_amount} {player_names}?",
+     "I am keeping {token_amount}",
+     "Help {player_names}",
+     "Help {player_names}?",
+     "{player_names} need to defend",
+     "To protect {player_names}",
+     "To be subtle",
+     "Before {player_names} attack"],
+
+    # Identifying actions
+    ["{player_names} are a group",
+     "{player_names} don't belong to a group",
+     "{player_names} want to join",
+     "{player_names} don't want to join",
+     "{player_names} are plotting an attack against {player_names}",
+     "{player_names} are not plotting an attack against {player_names}",
+     "{player_names} might attack {player_names}",
+     "{player_names} attacked {player_names}",
+     "{player_names} gave to {player_names}",
+     "{player_names} boosted {player_names}",
+     "{player_names} recruited {player_names}",
+     "{player_names} tried to recruit {player_names}",
+     "{player_names} might hold {token_amount}",
+     "{player_names} held",
+     "{player_names} has been giving"],
+
+    # Identifying characteristics
+    ["{player_names} is cheap",
+     "{player_names} is untrustworthy",
+     "{player_names} is violent",
+     "{player_names} is bad",
+     "{player_names} is good",
+     "{player_names} is peaceful",
+     "{player_names} is friendly",
+     "{player_names} is honest",
+     "{player_names} is strong",
+     "{player_names} is weak",
+     "{player_names} is connected",
+     "{player_names} is isolated",
+     "{player_names} is the strongest group",
+     "{player_names} would get stronger"],
+
+    # Clarification questions
+    ["Why?",
+     "{player_names} how much are you sending?",
+     "{player_names} how much are you stealing?",
+     "{player_names} how much are you keeping?",
+     "{player_names} why aren't you giving?",
+     "{player_names} why aren't you attacking?",
+     "{player_names} why are you giving less?",
+     "How can I trust {player_names}?",
+     "How much are you sending to {player_names}?",
+     "Why are you helping {player_names}?",
+     "Why are you attacking {player_names}?",
+     "Are {player_names} trading with {player_names}?",
+     "Are {player_names} talking with {player_names}?",
+     "Are {player_names} friends?",
+     "Who should I give to?",
+     "Who should I attack?",
+     "What happened {player_names}?",
+     "What now?",
+     "What is {player_names} doing?",
+     "{player_names} thoughts?",
+     "What are {player_names} doing this round?",
+     "Are {player_names} sure?"],
+
+    # Explanations for bad behavior
+    ["I don't have enough tokens",
+     "I missed the message",
+     "The time ran out",
+     "I forgot to submit",
+     "I messed up",
+     "I misunderstood",
+     "I misspoke",
+     "Wrong group message",
+     "Sorry"],
+
+    # Short responses
+    ["Yes",
+     "Agreed",
+     "Sounds good",
+     "Next round",
+     "Not now",
+     "Later",
+     "No",
+     "Thanks",
+     "All good",
+     "Nevermind",
+     "No problem",
+     "True",
+     "Good game",
+     "Shoot",
+     "Darn",
+     "It's working",
+     "That worked",
+     "That didn't work",
+     "Me too",
+     "Same as last round"]
+]
+
+# Main types of messages to be predicted
+message_type_classes = ['building relationships', 'destroying relationships', 'defense', 'identifying actions',
+                        'identifying characteristics', 'clarification questions', 'explanations for bad behavior',
+                        'short responses']
+
+# For each main message type, create labels for the submessage types
+classes, sub_class_label = {}, len(message_type_classes)
+
+for i, message_type in enumerate(message_type_classes):
+    classes[message_type] = {'label': i, 'subclasses': {}}
+    sub_classes = all_classes[i]
+
+    for sub_class in sub_classes:
+        classes[message_type]['subclasses'][sub_class] = sub_class_label
+        sub_class_label += 1
+
+# for key, vals in classes.items():
+#     print(f'{key}, {vals["label"]}:')
+
+#     for sub_message_key, sub_message_val in vals['subclasses'].items():
+#         print(f'{sub_message_key}, {sub_message_val}')
+#     print()
